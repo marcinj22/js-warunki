d029f7a21286c99829dd693c1d08804fbdc8213e
marcinj <marcin.jurewicz@gmail.com>
2022-07-29 14:05:50 +0200

    poprawa zadania

--- js/main.js
+++ js/main.js
@@ -2,8 +2,14 @@ let number = 12;
 
 let nmr = 10;
 
-if (number > 10); {
-    console.log('Podana liczba jest większa od 10');
+function higherNumber(number, nmr) {
+    if (number > 10) {
+        return "Podana liczba jest większa od 10";
+    }   else {
+        return "Podana liczba nie jest większa od 10";    
 }
+}
+
+console.log(higherNumber(number, nmr));
 
 
